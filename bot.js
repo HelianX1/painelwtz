6f41413a15e9a464ca5317659d4fe7e078468466
HelianX1 <143973877+HelianX1@users.noreply.github.com>
2024-11-22 15:22:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ const { Client } = require('whatsapp-web.js');
 
 const client = new Client();
 
-palavras = ['Tela','Display','Bateria','Carcaça','carcasa','do','4g','5g','?', '!', '.', ',', 'Dock','Doc','Moto','Xiaomi','Motorola'];
 
 client.on('ready', () => {
     console.log('Client is ready!');
@@ -16,19 +15,12 @@ client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
 });
 client.on('message', message => {
-
-    // reply back "pong" directly to the message
-    console.log(message.author+'Mensagem recebida: ' + message.body);
-    // remover caracteris especiais mensagem.body
-    message.body = message.body.replace(/[^a-zA-Z0-9]/g, '');
-    // remover palavras
-    palavras.forEach(palavra => {
-        message.body = message.body.replace(palavra, '');
-    });
-    
+    console.log('Mensagem recebida: ', message.body);
+    console.log('http://localhost/painelwtz/api.php?id_loja=2&produto=' + message.body);
     request('http://localhost/painelwtz/api.php?id_loja=2&produto=' + message.body, function (error, response, body) {
- 
-            message.reply(body.trim());
+
+            message.reply(body);
+            console.log(body);
     });
 });
 
